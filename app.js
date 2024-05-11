cc09e91471da2b18b4f140d253140d89a32c38b1
SaifAlisoomro <hk4948171@gmail.com>
2024-05-11 17:34:45 +0500

    Alias type

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+let myBio = {
+    name: "Saif SOOMRO",
+    age: 26,
+    roll: "Student at GIAIC",
+    phone: 123456789
+};
+console.log(myBio);
---
+++ app.ts
@@ -0,0 +1,14 @@
+type data = {
+    name:string,
+    age:number,
+    roll:string,
+    phone:number
+};
+ 
+let myBio: data = {
+    name:"Saif SOOMRO",
+    age:26,
+    roll:"Student at GIAIC",
+    phone:123456789
+}
+console.log(myBio);
